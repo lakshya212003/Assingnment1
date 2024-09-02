e51a400d57f66fa9afca4c3a7816cefb8738cdfe
lakshya212003 <134777386+lakshya212003@users.noreply.github.com>
2024-09-02 22:41:41 +0530

    Add files via upload

---
+++ hello1.py.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import platform
+name = "LAKSHYA JOSHI"
+course = "MCA with specialization in Data Science"
+
+print("Name:", name)
+print("Course:", course)
+print("Python Version:", platform.python_version())
+
+
+# In[ ]:
+
+
+
+
